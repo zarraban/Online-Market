ef12e70c994c32509b553231e2937c3382c2561e
Smyrnov Vlad <vladsimovv@gmail.com>
2025-07-17 14:26:33 +0300

    Add dto for entity User

---
+++ src/main/java/com/example/Online_Market/dto/UserDto.java
@@ -0,0 +1,49 @@
+package com.example.Online_Market.dto;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    @NotNull
+    @Size(min = 2, max = 15)
+    @NotBlank
+    private String firstName;
+
+    @NotNull
+    @Size(min = 5, max = 15)
+    @NotBlank
+    private String lastName;
+
+    @NotNull
+    @NotBlank
+    @Email
+    private String email;
+
+
+    @NotNull
+    @NotBlank
+    private String phone;
+
+
+    @NotNull
+    @NotBlank
+    private String country;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 5, max = 15)
+    private String password;
+
+}
