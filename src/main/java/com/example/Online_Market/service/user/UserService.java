3607fae84241958f7501bd7700ae9eb43d01ba71
Smyrnov Vlad <vladsimovv@gmail.com>
2025-07-18 19:28:19 +0300

    Add updateUserPhoto() method (+TODO logger)

--- src/main/java/com/example/Online_Market/service/user/UserService.java
+++ src/main/java/com/example/Online_Market/service/user/UserService.java
@@ -2,13 +2,16 @@ package com.example.Online_Market.service.user;
 
 import com.example.Online_Market.dto.UserDto;
 import com.example.Online_Market.entity.user.User;
+import com.example.Online_Market.exception.FileException;
 import com.example.Online_Market.repository.role.RoleRepository;
 import com.example.Online_Market.repository.user.UserRepository;
 import com.example.Online_Market.service.BaseService;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.io.File;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -33,6 +36,7 @@ public class UserService implements BaseService<UserDto, User> {
 
 
     @Override
+    @Transactional
     public User save(UserDto entity) {
         Objects.requireNonNull(entity, "Parameter [entity] must not be null!");
         
@@ -51,21 +55,54 @@ public class UserService implements BaseService<UserDto, User> {
     }
 
     @Override
+    @Transactional
     public boolean deleteByEmail(String email) {
         Objects.requireNonNull(email, "Parameter [email] must not be null!");
         return userRepository.deleteByEmail(email);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Optional<List<User>> getAll() {
         List<User> users = userRepository.findAll();
         return Optional.of(users);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Optional<User> getByEmail(String email) {
         Objects.requireNonNull(email, "Parameter [email] must not be null!");
         User user = userRepository.findByEmail(email);
         return Optional.of(user);
+    }
+
+
+    @Transactional
+    public boolean updateUserPhoto(String email, String photoPath){
+        Objects.requireNonNull(email, "Parameter [email] must not be null!");
+        Objects.requireNonNull(photoPath, "Parameter [photoPath] must not be null!");
+
+        File file = new File(photoPath);
+        if(!file.exists() || !file.isFile()){
+
+            //TODO add logs
+            throw new FileException("FIle doesn't exist or not valid!");
+        }
+
+        User user = userRepository.findByEmail(email);
+        if(user==null){
+            //TODO add logs
+            throw new FileException("User with email= "+ email +" was not found!");
+        }
+
+        try{
+            user.setProfilePhoto(photoPath);
+            userRepository.save(user);
+            return true;
+        }catch (Exception e){
+            //TODO add logger with this message
+            return false;
+        }
+
     }
 }
