cdd2d2bd27880d9edb0d19d0e662a5d6ab024f94
Smyrnov Vlad <vladsimovv@gmail.com>
2025-07-18 18:09:11 +0300

    Change return type of getAll() to Optional<List<>>

--- src/main/java/com/example/Online_Market/service/BaseService.java
+++ src/main/java/com/example/Online_Market/service/BaseService.java
@@ -1,10 +1,11 @@
 package com.example.Online_Market.service;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BaseService<T1, T2>{
     void save(T1 entity);
     boolean deleteByEmail(String email);
-    Optional<T2> getAll();
+    Optional<List<T2>> getAll();
     Optional<T2> getByEmail(String email);
 }
