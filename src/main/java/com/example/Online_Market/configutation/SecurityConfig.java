d40b60affd290a2547c2a963892f56a2aacdaf0a
Smyrnov Vlad <vladsimovv@gmail.com>
2025-08-03 10:40:08 +0300

    Add CompromisedPasswordChecker realization(HaveIBeenPwnedRestApiPasswordChecker)

--- src/main/java/com/example/Online_Market/configutation/SecurityConfig.java
+++ src/main/java/com/example/Online_Market/configutation/SecurityConfig.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -47,5 +49,10 @@ public class SecurityConfig {
                         .successForwardUrl("/main"));
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public CompromisedPasswordChecker passwordChecker(){
+        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 }
