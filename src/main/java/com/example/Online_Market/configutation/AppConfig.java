a586db0ed00d811f56454caf4c1f64389f345ad1
Smyrnov Vlad <vladsimovv@gmail.com>
2025-09-02 10:00:09 +0300

    Add WebClient configuration(api-key+base-url)

--- src/main/java/com/example/Online_Market/configutation/AppConfig.java
+++ src/main/java/com/example/Online_Market/configutation/AppConfig.java
@@ -1,16 +1,34 @@
 package com.example.Online_Market.configutation;
 
 import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 @ComponentScan(value = "com.example.Online_Market")
 public class AppConfig {
+
+    @Value("${ninja.api.base-url}")
+    private String baseUrlNinjaApi;
+
+    @Value("${x.api.key}")
+    private String xApiKey;
+
+
     // Layout dialect Bean for proper work of Thymeleaf Layout Dialect
     @Bean
     public LayoutDialect layoutDialect(){
         return new LayoutDialect();
+    }
+
+    @Bean
+    public WebClient api(WebClient.Builder builder){
+        return builder
+                .baseUrl(baseUrlNinjaApi)
+                .defaultHeader("X-Api-Key", xApiKey)
+                .build();
     }
 }
