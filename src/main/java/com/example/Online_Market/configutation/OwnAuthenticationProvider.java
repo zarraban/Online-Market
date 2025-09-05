2cc7bd5b82f44f4f846440b05257f861fc731908
Smyrnov Vlad <vladsimovv@gmail.com>
2025-09-05 23:35:19 +0300

    Add @Component annotation to OwnAuthenticationProvider

--- src/main/java/com/example/Online_Market/configutation/OwnAuthenticationProvider.java
+++ src/main/java/com/example/Online_Market/configutation/OwnAuthenticationProvider.java
@@ -1,16 +1,18 @@
 package com.example.Online_Market.configutation;
 
-import com.example.Online_Market.service.user.UserService;
+
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.parameters.P;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
+
+@Component
 public class OwnAuthenticationProvider implements AuthenticationProvider {
 
     private PasswordEncoder passwordEncoder;
