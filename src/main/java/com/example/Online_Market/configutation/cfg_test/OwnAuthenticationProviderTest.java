ec30c281c05ccd8f1ee04c2ccea843267b1c33e1
Smyrnov Vlad <vladsimovv@gmail.com>
2025-09-12 22:29:58 +0300

    Add OwnAuthenticationProviderTest for test Profile

---
+++ src/main/java/com/example/Online_Market/configutation/cfg_test/OwnAuthenticationProviderTest.java
@@ -0,0 +1,55 @@
+package com.example.Online_Market.configutation.cfg_test;
+
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Profile("!prod")
+public class OwnAuthenticationProviderTest implements AuthenticationProvider {
+
+    private PasswordEncoder passwordEncoder;
+    private UserDetailsService userService;
+
+    public OwnAuthenticationProviderTest(
+            UserDetailsService userService,
+            PasswordEncoder passwordEncoder
+    ){
+        this.passwordEncoder = passwordEncoder;
+        this.userService = userService;
+    }
+    /**
+     * @param authentication the authentication request object.
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        UserDetails userDetails = userService.loadUserByUsername(username);
+            return new UsernamePasswordAuthenticationToken(
+                    username,
+                    password,
+                    userDetails.getAuthorities());
+
+    }
+
+    /**
+     * @param authentication
+     * @return
+     */
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
