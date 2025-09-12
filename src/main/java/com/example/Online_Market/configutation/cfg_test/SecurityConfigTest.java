0cfc2ecd465e4944e0aa877aa3021b8f781b1c1c
Smyrnov Vlad <vladsimovv@gmail.com>
2025-09-12 22:30:33 +0300

    Add SecurityConfigTest for test Profile

---
+++ src/main/java/com/example/Online_Market/configutation/cfg_test/SecurityConfigTest.java
@@ -0,0 +1,60 @@
+package com.example.Online_Market.configutation.cfg_test;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@Profile("!prod")
+public class SecurityConfigTest {
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        //FOR TESTING
+//        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+//        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+        http
+                .authorizeHttpRequests((request)->
+                request.requestMatchers(
+                        "/registration",
+                        "/login",
+                        "/error",
+                        "/main",
+                        "contact",
+                        "/registration/save/user",
+                        "/carouselPhotos/**",
+                        "/footerPhotos/**",
+                        "/logo/**",
+                        "/siteIcon/**",
+                        "/userPhotos/**",
+                        "/css/**",
+                        "/js/**").permitAll());
+        http.formLogin((login)->
+                login.loginPage("/login")
+                        .successForwardUrl("/main"));
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public CompromisedPasswordChecker passwordChecker(){
+        //Realization of CompromisedPasswordChecker to check whether you password is not strong or good enough
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+}
