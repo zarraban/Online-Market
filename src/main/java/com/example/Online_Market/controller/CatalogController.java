0a60d2a05d573bde06d1eeca969b2a463f614005
Smyrnov Vlad <vladsimovv@gmail.com>
2025-09-16 14:22:34 +0300

    Add check(whether user isAuthenticated) for layouts in CatalogController

--- src/main/java/com/example/Online_Market/controller/CatalogController.java
+++ src/main/java/com/example/Online_Market/controller/CatalogController.java
@@ -1,8 +1,25 @@
 package com.example.Online_Market.controller;
 
 
+import lombok.Getter;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CatalogController {
+
+
+
+    @GetMapping("/catalog")
+    public String catalogPage(Model model, Authentication authentication){
+        boolean isAuth = authentication!=null && authentication.isAuthenticated();
+        if(isAuth){
+            model.addAttribute("layout", "/layouts/layoutLogged.html");
+        }else {
+            model.addAttribute("layout", "/layouts/layout.html");
+        }
+        return "catalog";
+    }
 }
