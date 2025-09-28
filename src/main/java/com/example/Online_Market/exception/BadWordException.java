a1b07221afb9e53f70db0535f54e36f7daefc6ce
Smyrnov Vlad <vladsimovv@gmail.com>
2025-09-28 23:33:02 +0300

    Add BadWordException

---
+++ src/main/java/com/example/Online_Market/exception/BadWordException.java
@@ -0,0 +1,7 @@
+package com.example.Online_Market.exception;
+
+public class BadWordException extends RuntimeException {
+    public BadWordException(String message) {
+        super(message);
+    }
+}
