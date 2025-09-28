a0e384fcb1fad16587a363c92b408eaeb21bdd6d
Smyrnov Vlad <vladsimovv@gmail.com>
2025-09-28 23:31:46 +0300

    Add mapping has_property name to hasProperty field

--- src/main/java/com/example/Online_Market/dto/ProfanityResponseDto.java
+++ src/main/java/com/example/Online_Market/dto/ProfanityResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.Online_Market.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Getter
@@ -9,5 +10,15 @@ import lombok.*;
 public class ProfanityResponseDto {
     public String original;
     public String censored;
+    @JsonProperty("has_profanity")
     public Boolean hasProfanity;
+
+    @Override
+    public String toString() {
+        return "ProfanityResponseDto{" +
+                "original='" + original + '\'' +
+                ", censored='" + censored + '\'' +
+                ", hasProfanity=" + hasProfanity +
+                '}';
+    }
 }
